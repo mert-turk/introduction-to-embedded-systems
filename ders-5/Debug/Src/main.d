3bcf7d5892aca5f2035d9baabcb523ac0af63f55
mert-turk <mturkba@gmail.com>
2026-01-03 11:33:51 +0100

    vid5 is finished

---
+++ ders-5/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /home/mat/dersler/ders-5/drivers/device/stm32f446xx.h \
+ /home/mat/dersler/ders-5/drivers/CMSIS/core_cm4.h \
+ /home/mat/dersler/ders-5/drivers/CMSIS/cmsis_version.h \
+ /home/mat/dersler/ders-5/drivers/CMSIS/cmsis_compiler.h \
+ /home/mat/dersler/ders-5/drivers/CMSIS/cmsis_gcc.h \
+ /home/mat/dersler/ders-5/drivers/CMSIS/mpu_armv7.h \
+ /home/mat/dersler/ders-5/drivers/device/system_stm32f4xx.h
+/home/mat/dersler/ders-5/drivers/device/stm32f446xx.h:
+/home/mat/dersler/ders-5/drivers/CMSIS/core_cm4.h:
+/home/mat/dersler/ders-5/drivers/CMSIS/cmsis_version.h:
+/home/mat/dersler/ders-5/drivers/CMSIS/cmsis_compiler.h:
+/home/mat/dersler/ders-5/drivers/CMSIS/cmsis_gcc.h:
+/home/mat/dersler/ders-5/drivers/CMSIS/mpu_armv7.h:
+/home/mat/dersler/ders-5/drivers/device/system_stm32f4xx.h:
---
+++ ders-5/Src/main.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+#include "stm32f446xx.h"
+
+// GPIOA boundary start address
+//#define GPIOA 	 		0x40020000U
+// Reset and Clock Control start address
+//#define RCC				0x40023800U
+
+//#define GPIOA_MODER 	  (*(uint32_t volatile *)	(GPIOA + 0x00U))
+//#define GPIOA_OTYPER 	  (*(uint32_t volatile *)	(GPIOA + 0x04U))
+//#define GPIOA_PUPDR 	  (*(uint32_t volatile *)	(GPIOA + 0x0CU))
+//#define GPIOA_BSRR		  (*(uint32_t volatile *)	(GPIOA + 0x18U))
+//#define RCC_AHB1_GPIOA_EN (*(uint32_t volatile*)(RCC  +  0x30U))
+
+/* Functions Prototypes*/
+void delay(uint32_t tick);
+void userLedOff(void);
+void userLedOn(void);
+void gpioInit(void);
+
+
+void gpioInit(void)
+{
+	/*GPIOA Clock Enable*/
+	RCC->AHB1ENR |= (1U << 0U);
+	/* PA5 pin configuration*/
+	GPIOA->MODER &= ~(3U << 10U);
+
+	GPIOA->MODER |=  (1U << 10U); // Output mode
+
+	GPIOA->OTYPER &= ~(1u << 5U);
+
+	GPIOA->PUPDR &= ~(3U << 10U); // No pull up/down
+}
+
+
+
+int main(void)
+{
+
+
+	gpioInit();
+
+	while(1)
+	{
+		userLedOn();
+		delay(500000);
+		userLedOff();
+		delay(500000);
+	}
+}
+
+
+
+void delay(uint32_t tick)
+{
+	uint32_t volatile counter= 0;
+	while(counter < tick){
+		counter++;
+	}
+}
+
+void userLedOn(void)
+{
+	GPIOA->BSRR = (1U << 5U); // PA5 SET
+}
+
+void userLedOff(void)
+{
+	GPIOA->BSRR  = (1U << (5U+16U)); // PA5 RESET
+}
+
+
+
